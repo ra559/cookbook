e0b14abb6bba4c7ad340d024a6038811b91a8015
ra559 <arobertadrian@gmail.com>
2025-06-10 19:10:29 -0400

    added other dotfiles

---
+++ _recipes/Proxmox Notes.md
@@ -0,0 +1,5 @@
+
+# Regarding updates
+Enable no subscription repo: 
+![[Pasted image 20250610113008.png]]
+
---
+++ snipets/c/int_float1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int mega = 10000; 
+	float quarter = 0.25;
+	double larger = 10000000.0;
+	printf("Values:\n");
+	printf("Int: -\t%d\n",mega);
+	printf("Flt: -\t%f\n",quarter);
+	printf("Dob: -\t%f\n",larger);
+	return 0;
+}
